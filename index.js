a0dce6bbdf07e87795a8d7650fe890981ccf56de
Felistus Kimanthi <kimanthi.felistus@gmail.com>
2025-04-18 15:15:53 +0300

    functions added

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
 // Code your solution in this file!
+
+function distanceFromHqInBlocks(pickupLocation) {
+  const hqStreet = 42;
+  return Math.abs(pickupLocation - hqStreet);
+}
+
+function distanceFromHqInFeet(pickupLocation) {
+  const blocks = Math.abs(pickupLocation - 42);
+  return blocks * 264;
+}
+
+function distanceTravelledInFeet(start, destination) {
+  const blocksTravelled = Math.abs(destination - start);
+  return blocksTravelled * 264;
+}
+
+function calculatesFarePrice(start, destination) {
+  const distanceInFeet = Math.abs(destination - start) * 264;
+
+  if (distanceInFeet <= 400) {
+    return 0;
+  } else if (distanceInFeet > 400 && distanceInFeet <= 2000) {
+    return (distanceInFeet - 400) * 0.02;
+  } else if (distanceInFeet > 2000 && distanceInFeet <= 2500) {
+    return 25;
+  } else {
+    return "cannot travel that far";
+  }
+}
